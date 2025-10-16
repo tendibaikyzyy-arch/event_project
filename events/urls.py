b9e63d602b0a9cac90adc27b43f61b7a6cf7bedf
101ajf <tendibaikyzyy@mail.ru>
2025-10-17 01:03:59 +0500

    fixed urls connection for dashboard

--- events/urls.py
+++ events/urls.py
@@ -5,7 +5,13 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('login/', views.login_view, name='login'),
     path('register/', views.register_view, name='signup'),
-    path('dashboard/', views.dashboard_view, name='dashboard'),
+    path('logout/', views.logout_view, name='logout'),
+
+    path('dashboard/', views.dashboard, name='dashboard'),
     path('my-events/', views.my_events_view, name='my_events'),
     path('notifications/', views.notifications_view, name='notifications'),
+
+    path('create/', views.create_event, name='create_event'),
+    path('events-list/', views.events_list, name='events_list'),
+    path('register-event/<int:event_id>/', views.register_for_event, name='register_for_event'),
 ]
--- eventsystem/urls.py
+++ eventsystem/urls.py
@@ -1,11 +1,7 @@
-# eventsystem/urls.py
 from django.contrib import admin
 from django.urls import path, include
-from events import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home, name='home'),           # твоя главная (баннеры/карточки)
-    path('events/', include('events.urls')),     # если есть
-    path('dashboard/', views.dashboard, name='dashboard'),  # ЛК с календарём
+    path('', include('events.urls')),  # теперь все страницы из events будут работать
 ]
