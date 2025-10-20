720f8c230682990aef795a1a01333c3f3172f9a2
101ajf <tendibaikyzyy@mail.ru>
2025-10-20 18:04:07 +0500

    updated settings with redirect and static setup

--- events/urls.py
+++ events/urls.py
@@ -5,11 +5,13 @@ urlpatterns = [
     path('login/', views.login_view, name='login'),
     path('register/', views.register_view, name='register'),
     path('logout/', views.logout_view, name='logout'),
-    path('dashboard/', views.dashboard, name='dashboard'),
+    path('dashboard/', views.dashboard, name='dashboard'),  # 🟢 басты бет — календарь
     path('create/', views.create_event, name='create_event'),
     path('events-list/', views.events_list, name='events_list'),
     path('register-event/<int:event_id>/', views.register_for_event, name='register_for_event'),
     path('my-events/', views.my_events, name='my_events'),
     path('notifications/', views.notifications, name='notifications'),
-    path('', views.home, name='home'),
+
+    # ❗ Басты маршрут та осылай dashboard-қа бағытталу керек
+    path('', views.dashboard, name='home'),
 ]
