de519a0942b0925d01370a8d67af4287f7004557
101ajf <tendibaikyzyy@mail.ru>
2025-10-20 20:12:03 +0500

    updated views with dashboard, events, notifications

--- eventsystem/urls.py
+++ eventsystem/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth import views as auth_views  # ✅ Мына жолды қостық
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('events.urls')),
+    path('accounts/login/', auth_views.LoginView.as_view(template_name='events/login.html'), name='account_login'),  # ✅ Фикс
+    path('', include('events.urls')),  # Барлық маршруттар events ішінен
 ]
