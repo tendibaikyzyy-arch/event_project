a2f89c642781000649c3a2e86209b2dd34aa8cb4
101ajf <tendibaikyzyy@mail.ru>
2025-10-22 20:57:08 +0500

    updated views with dashboard, events, notifications

--- events/urls.py
+++ events/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('login/', views.login_view, name='login'),
-    path('register/', views.register_view, name='register'),
+    path('register/', views.register, name='register'),
     path('logout/', views.logout_view, name='logout'),
     path('dashboard/', views.dashboard, name='dashboard'),
     path('create/', views.create_event, name='create_event'),
