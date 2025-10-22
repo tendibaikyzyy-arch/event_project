c24cf9a49f85f53e9d6a17ab6dda0a094b6e0763
101ajf <tendibaikyzyy@mail.ru>
2025-10-22 21:00:31 +0500

    updated views with dashboard, events, notifications

--- events/views.py
+++ events/views.py
@@ -3,6 +3,7 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.http import JsonResponse
+from django.contrib import messages   # ✅ МІНДЕТТІ
 from .models import Event
 
 def home(request):
