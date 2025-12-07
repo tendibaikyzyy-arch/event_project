bb920bfa6f2075dc6acc253bbba7a5d5e5a867e2
101ajf <tendibaikyzyy@mail.ru>
2025-12-07 15:02:35 +0500

    Fix reports and admin feedback view

--- events/admin.py
+++ events/admin.py
@@ -1,32 +1,31 @@
 from django.contrib import admin
 from .models import Event, Registration, Notification, Feedback
 
-
 @admin.register(Event)
 class EventAdmin(admin.ModelAdmin):
-    list_display = ("title", "date", "time", "place", "capacity", "created_by")
-    list_filter = ("date", "place", "created_by")
-    search_fields = ("title", "description")
-
+    list_display = ('title', 'date', 'time', 'place', 'capacity', 'created_by')
+    list_filter = ('date',)
+    search_fields = ('title', 'description', 'place')
 
 @admin.register(Registration)
 class RegistrationAdmin(admin.ModelAdmin):
-    list_display = ("user", "event", "created_at", "attended", "rating")
-    list_filter = ("attended", "event")
-    search_fields = ("user__username", "event__title")
-    list_editable = ("attended", "rating")  # можно менять прямо в списке
-    ordering = ("-created_at",)
-
+    list_display = ('user', 'event', 'created_at', 'attended', 'rating')
+    list_filter = ('event', 'attended', 'rating')
+    search_fields = ('user__username', 'event__title')
 
 @admin.register(Notification)
 class NotificationAdmin(admin.ModelAdmin):
-    list_display = ("user", "title", "created_at", "is_read")
-    list_filter = ("is_read", "created_at")
-    search_fields = ("user__username", "title")
-
+    list_display = ('user', 'title', 'is_read', 'created_at')
+    list_filter = ('is_read',)
+    search_fields = ('user__username', 'title', 'body')
 
 @admin.register(Feedback)
 class FeedbackAdmin(admin.ModelAdmin):
-    list_display = ("event", "user", "rating", "created_at")
-    list_filter = ("event", "rating")
-    search_fields = ("comment", "reply", "user__username")
+    list_display = ('event', 'user', 'rating', 'created_at', 'has_reply')
+    list_filter = ('event', 'rating')
+    search_fields = ('user__username', 'comment')
+
+    def has_reply(self, obj):
+        return bool(obj.reply)
+    has_reply.boolean = True
+    has_reply.short_description = 'Ответ'
